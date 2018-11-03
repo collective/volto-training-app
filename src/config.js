3d79bc79eb1b901554ef94877d87537b8d637328
Rob Gietema <rob.gietema@gmail.com>
2018-11-03 20:21:24 +0900

    Added visual editor settings.

--- src/config.js
+++ src/config.js
@@ -12,6 +12,12 @@
  * }
  */
 
+import React from 'react';
+import createInlineStyleButton from 'draft-js-buttons/lib/utils/createInlineStyleButton';
+import Icon from '@plone/volto/components/theme/Icon/Icon';
+import underlineSVG from '@plone/volto/icons/underline.svg';
+import codeSVG from '@plone/volto/icons/code.svg';
+
 import {
   settings as defaultSettings,
   views as defaultViews,
@@ -21,8 +27,23 @@ import {
 
 import { AlbumView, FullView, RatingWidget } from './components';
 
+const UnderlineButton = createInlineStyleButton({
+  style: 'UNDERLINE',
+  children: <Icon name={underlineSVG} size="24px" />,
+});
+
+const CodeButton = createInlineStyleButton({
+  style: 'CODE',
+  children: <Icon name={codeSVG} size="24px" />,
+});
+
 export const settings = {
   ...defaultSettings,
+  richTextEditorInlineToolbarButtons: [
+    CodeButton,
+    UnderlineButton,
+    ...defaultSettings.richTextEditorInlineToolbarButtons,
+  ],
 };
 
 export const views = {
